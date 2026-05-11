367c18fa2463a5732be410ab4c9eeae8a1f991a0
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-11 13:01:21 +0200

    Allow runtime-patched app symbols in lint

--- eslint.config.js
+++ eslint.config.js
@@ -14,4 +14,13 @@ export default [
       },
     },
   },
+  {
+    files: ['src/js/app.js'],
+    rules: {
+      // app.js is loaded as raw source and patched by app-life-bootstrap-v3.js.
+      // These symbols are intentionally provided/used by that runtime patch layer.
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+    },
+  },
 ];
