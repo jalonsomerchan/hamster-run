4393884a890b6b3775181af3b28fa8948ed72a80
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-14 20:22:21 +0200

    Add time difficulty curve config

---
+++ src/js/config/difficultyCurve.js
@@ -0,0 +1,12 @@
+export const TIME_DIFFICULTY_CURVE = {
+  fastRampStartSeconds: 30,
+  earlyDifficultyPerSecond: 0.012,
+  lateDifficultyPerSecond: 0.04,
+  earlySpeedPerSecond: 1.8,
+  lateSpeedPerSecond: 5.2,
+  earlyEnemyPerSecond: 0.004,
+  lateEnemyPerSecond: 0.014,
+  maxDifficultyBonus: 1.35,
+  maxSpeedBonus: 220,
+  maxEnemyBonus: 0.52,
+};
